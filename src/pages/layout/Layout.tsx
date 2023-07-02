c370399a552ddd9ec246187134dd18d54462f9d6
pratyushplp <pratyushplp@gmail.com>
2023-07-01 20:23:07 -0500

    Added navbar with logo to layout page.

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,8 +1,6 @@
-
+import { Outlet, NavLink, Link } from "react-router-dom";
+import ajgLogo from '../../assets/ajgLogo.png'
 import styles from "./Layout.module.css";
-
-
-
 
 
 
@@ -12,6 +10,30 @@ const Layout = () => {
     return (
 
         <div className={styles.layout}>
+            <header className={styles.header} >
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                       <img src={ajgLogo} alt="icon" className={styles.logo}/>
+                        <h3 className={styles.headerTitle}>Gallagher</h3>
+                    </Link>
+                    <nav className={styles.headerNavList}>
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Chat
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+
+            <Outlet />
         </div>
     );
 };
